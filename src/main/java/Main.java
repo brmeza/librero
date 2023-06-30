484de9845a20ab70fa3eb623308fdc115585d08a
brmeza <brayanmesz1998@gmail.com>
2023-06-29 21:59:58 -0500

    calculadora v1

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+        Raices r1 = new Raices(1.0, 4.0, 4.0);
+        r1.calcular();
+    }
+}
